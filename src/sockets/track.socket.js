6d722fcc734ba4ae18a1abd52bb1d892b305138a
Jair Arturo Pacheco Grau <jaapache@nequi.com>
2026-03-12 11:00:40 -0500

    Remove mongoose dependency from track.socket.js

--- src/sockets/track.socket.js
+++ src/sockets/track.socket.js
@@ -1,12 +1,8 @@
 const Track = require('../models/Track');
 const logger = require('../utils/logger');
-const mongoose = require('mongoose');
 
 // In-memory storage for tracks
 const memoryTracks = new Map();
-
-// Check if MongoDB is connected
-const isMongoConnected = () => mongoose.connection.readyState === 1;
 
 module.exports = (io, socket) => {
   
@@ -14,46 +10,33 @@ module.exports = (io, socket) => {
     const { waveId, title, artist, duration } = data;
     
     try {
-      if (isMongoConnected()) {
-        // Mark previous current track as not current
-        await Track.updateMany(
-          { waveId, isCurrent: true },
-          { isCurrent: false }
-        );
-        
-        // Create new current track
-        const track = new Track({
-          waveId,
-          title,
-          artist,
-          duration,
-          isCurrent: true
-        });
-        
-        await track.save();
-      } else {
-        // Use memory storage
-        const trackId = `track_${Date.now()}`;
-        const track = {
-          id: trackId,
-          waveId,
-          title,
-          artist,
-          duration,
-          isCurrent: true,
-          playedAt: new Date()
-        };
-        
-        // Mark previous tracks as not current
-        const waveTracksKey = `wave_${waveId}`;
-        if (!memoryTracks.has(waveTracksKey)) {
-          memoryTracks.set(waveTracksKey, []);
-        }
-        
-        const waveTracks = memoryTracks.get(waveTracksKey);
-        waveTracks.forEach(t => t.isCurrent = false);
-        waveTracks.unshift(track);
+      // Create new current track in DynamoDB
+      const track = await Track.create({
+        waveId,
+        title,
+        artist,
+        duration,
+        isCurrent: true
+      });
+      
+      // Also store in memory
+      const trackId = track.trackId;
+      const waveTracksKey = `wave_${waveId}`;
+      if (!memoryTracks.has(waveTracksKey)) {
+        memoryTracks.set(waveTracksKey, []);
       }
+      
+      const waveTracks = memoryTracks.get(waveTracksKey);
+      waveTracks.forEach(t => t.isCurrent = false);
+      waveTracks.unshift({
+        trackId,
+        waveId,
+        title,
+        artist,
+        duration,
+        isCurrent: true,
+        playedAt: new Date()
+      });
       
       // Broadcast to all listeners in the wave
       io.to(waveId).emit('current-track-updated', {
@@ -74,26 +57,10 @@ module.exports = (io, socket) => {
     const { waveId } = data;
     
     try {
-      let tracks = [];
-      
-      if (isMongoConnected()) {
-        tracks = await Track.find({ waveId })
-          .sort({ playedAt: -1 })
-          .limit(20);
-        
-        tracks = tracks.map(track => ({
-          title: track.title,
-          artist: track.artist,
-          duration: track.duration,
-          isCurrent: track.isCurrent,
-          playedAt: track.playedAt
-        }));
-      } else {
-        // Use memory storage
-        const waveTracksKey = `wave_${waveId}`;
-        const waveTracks = memoryTracks.get(waveTracksKey) || [];
-        tracks = waveTracks.slice(0, 20);
-      }
+      // Get tracks from memory (DynamoDB queries would be expensive)
+      const waveTracksKey = `wave_${waveId}`;
+      const waveTracks = memoryTracks.get(waveTracksKey) || [];
+      const tracks = waveTracks.slice(0, 20);
       
       socket.emit('wave-tracks', {
         waveId,
